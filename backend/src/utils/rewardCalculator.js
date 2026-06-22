0e23ebfd92314395c338e6ffa67b83c47176e3d2
ludogx1-cpu <ludogx1@gmail.com>
2026-06-22 15:07:31 +0100

    Update faucet base reward to 0.004

--- backend/src/utils/rewardCalculator.js
+++ backend/src/utils/rewardCalculator.js
@@ -1,11 +1,5 @@
 function calculateDogeReward(price) {
-  if (price <= 0.05) {
-    return 0.0016;
-  } else if (price >= 0.50) {
-    return 0.0004;
-  } else {
-    return 0.0016 - ((price - 0.05) / 0.45) * 0.0012;
-  }
+  return 0.004;
 }
 
 module.exports = {
