fda3d35f3f188f33c908ac934972d3d914ebd5f1
ludogx1-cpu <ludogx1@gmail.com>
2026-06-26 08:35:41 +0100

    Update faucet max limit to 0.002 and min limit to 0.0002

--- backend/src/utils/rewardCalculator.js
+++ backend/src/utils/rewardCalculator.js
@@ -1,10 +1,10 @@
 function calculateDogeReward(price) {
   if (price <= 0.02) {
-    return 0.01;
+    return 0.002;
   } else if (price >= 0.20) {
-    return 0.001;
+    return 0.0002;
   } else {
-    return 0.01 - ((price - 0.02) / 0.18) * 0.009;
+    return 0.002 - ((price - 0.02) / 0.18) * 0.0018;
   }
 }
 
