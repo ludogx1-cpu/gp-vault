78f06fe4405c85bd12247d6f37143830168160e0
ludogx1-cpu <ludogx1@gmail.com>
2026-07-25 05:38:46 +0100

    Add Firebase Performance Monitoring

--- lib/src/firebase_service.dart
+++ lib/src/firebase_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +27,9 @@ class FirebaseService {
     } else {
       await Firebase.initializeApp();
     }
+
+    // Initialize Performance Monitoring
+    FirebasePerformance.instance;
 
     // Push Notifications setup
     try {
